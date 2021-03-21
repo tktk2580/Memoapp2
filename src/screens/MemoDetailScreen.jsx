c74da0a95510625710a6ca0be12f59a243499bb9
takeshi-katamura <takeshi-katamura@giginc.co.jp>
2021-03-21 20:01:49 +0900

    install expo vector icon

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import {
+  View, ScrollView, Text, StyleSheet,
+} from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
@@ -18,7 +20,7 @@ export default function MemoDetailScreen() {
           書体や書体や書体や書体や書体や書体や書体や書体や書体や書体や書体や
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+      <CircleButton style={{ top: 160, bottom: 'auto' }} name="edit" />
     </View>
   );
 }
