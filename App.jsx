1c23969787c7ee86b088a8db040e562e3e2041a8
takeshi-katamura <takeshi-katamura@giginc.co.jp>
2021-03-22 09:29:01 +0900

    login signup screen

--- App.jsx
+++ App.jsx
@@ -4,13 +4,16 @@ import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
-
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
     // <MemoCreateScreen />
+    // <LogInScreen />
+    <SignUpScreen />
   );
 }
